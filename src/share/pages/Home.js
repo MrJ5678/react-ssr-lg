2f7bd42ce41772668e62a0f46d9cc67f5dc5914a
hhhhhq <Mr.j@tom.com>
2022-05-25 09:31:59 +0800

    init

---
+++ src/share/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react=router-dom'
+
+function Home() {
+  return (
+    <div onClick={() => console.log('hello')}>
+      Home works
+      <Link to="/list">jump to list</Link>
+    </div>
+  )
+}
+
+export default Home
